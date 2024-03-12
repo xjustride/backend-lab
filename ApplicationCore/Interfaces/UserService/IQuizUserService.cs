62c8056be5351f353ec1bcbfe5492372c451daea
Remigiusz <remigiusz.krupa@microsoft.wsei.edu.pl>
2024-03-13 00:53:04 +0100

    init

---
+++ ApplicationCore/Interfaces/UserService/IQuizUserService.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.Models;
+using ApplicationCore.Models.QuizAggregate;
+
+namespace ApplicationCore.Interfaces.UserService;
+
+public interface IQuizUserService
+{
+    Quiz CreateAndGetQuizRandom(int count);
+
+    Quiz? FindQuizById(int id);
+
+    void SaveUserAnswerForQuiz(int quizId, int userId, int quizItemId, string answer);
+
+    List<QuizItemUserAnswer> GetUserAnswersForQuiz(int quizId, int userId);
+
+    int CountCorrectAnswersForQuizFilledByUser(int quizId, int userId)
+    {
+        return GetUserAnswersForQuiz(quizId, userId)
+            .Count(e => e.IsCorrect());
+    }
+}
