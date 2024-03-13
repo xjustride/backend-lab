27827fa4135e7cbe3e0a2ca17f5bf7c63916260f
Remigiusz <remigiusz.krupa@microsoft.wsei.edu.pl>
2024-03-13 11:10:06 +0100

    adding UserService class, dictionarny and method logic

---
+++ BlazorServer/UserService.cs
@@ -0,0 +1,34 @@
+﻿namespace BlazorServer
+{
+    public class UserService
+    {
+        private readonly Dictionary<string, string> _users = new Dictionary<string, string>();
+        public void Add(string connectionId, string username)
+        { 
+            _users.Add(connectionId, username);
+
+        }
+
+        public void RemoveByName(string username)
+        {
+            foreach (var (id, name) in _users) 
+            {
+                if(name == username)
+                {
+                    _users.Remove(id);
+                }
+            }
+        }
+
+        public string GetConnectionIdByName(string username)
+        {
+            string connectionId = string.Empty;
+            return connectionId;
+        }
+
+        public IEnumerable<(string ConnectionId, string Username)> GetAll()
+        {
+
+        }
+    }
+}
