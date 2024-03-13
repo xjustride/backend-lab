8b60ef9ba59985b2bb7839102e4a4d736862a501
Remigiusz <remigiusz.krupa@microsoft.wsei.edu.pl>
2024-03-13 19:21:21 +0100

    updating and fixing repo

---
+++ BlazorServer/UserService.cs
@@ -0,0 +1,41 @@
+﻿namespace BlazorServer
+{
+    public class UserService
+    {
+        private readonly Dictionary<string, string> _users = new Dictionary<string, string>();
+        public void Add(string connectionId, string username)
+        { 
+            _users.Add(connectionId, username);
+
+        }
+
+        public void RemoveByName(string username)
+        {
+            foreach (var (id, name) in _users) 
+            {
+                if(name == username)
+                {
+                    _users.Remove(id);
+                }
+            }
+        }
+
+        public string GetConnectionIdByName(string username)
+        {
+            foreach (var (id, name) in _users)
+            {
+                if (name == username)
+                {
+                    return id;
+                }
+            }
+
+            return "";
+        }
+
+        public IEnumerable<(string ConnectionId, string Username)> GetAll()
+        {
+             return _users.Select(u => (u.Value, u.Key));
+        }
+    }
+}
