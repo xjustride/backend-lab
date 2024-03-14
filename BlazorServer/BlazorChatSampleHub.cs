cd7b7ca168ec03a83c546cf903e34839e8cfd07c
Remigiusz <remigiusz.krupa@microsoft.wsei.edu.pl>
2024-03-14 23:26:18 +0100

    update add and remove method in chatroom.razor

--- BlazorServer/BlazorChatSampleHub.cs
+++ BlazorServer/BlazorChatSampleHub.cs
@@ -20,4 +20,9 @@ public class BlazorChatSampleHub : Hub
         Console.WriteLine($"Disconnected {e?.Message} {Context.ConnectionId}");
         await base.OnDisconnectedAsync(e);
     }
+
+    public async Task Private(string from, string to, string message)
+    {
+        await Clients.Client(to).SendAsync("Private", from, message);
+    }
 }
